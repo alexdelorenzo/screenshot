a5f773f479e86e7dcac5cfd87bffa8929feaa74d
thismachinechills <alex.delorenzo@gmail.com>
2015-03-08 14:49:59 -0400

    init

---
+++ get_window_id.py
@@ -0,0 +1,52 @@
+try:
+    from Quartz import CGWindowListCopyWindowInfo, kCGWindowListExcludeDesktopElements, kCGNullWindowID, kCGWindowNumber, \
+                       kCGWindowName, kCGWindowOwnerName, kCGWindowListOptionAll, kCGWindowListOptionOnScreenOnly, \
+                       kCGWindowListOptionOnScreenAboveWindow, kCGWindowListOptionOnScreenBelowWindow, \
+                       kCGWindowListOptionIncludingWindow
+
+except ImportError as ex:
+    raise Exception("Please install pyobjc-framework-Quartz via pip.") from ex
+
+
+options = {'all_windows': kCGWindowListOptionAll,
+           'on_screen_only': kCGWindowListOptionOnScreenOnly,
+           'above_window': kCGWindowListOptionOnScreenAboveWindow,
+           'below_window': kCGWindowListOptionOnScreenBelowWindow,
+           'include_window': kCGWindowListOptionIncludingWindow,
+           'exclude_desktop': kCGWindowListExcludeDesktopElements}
+
+user_options_str = 'exclude_desktop on_screen_only'
+
+summer = lambda *opts: sum(options[opt] for opt in opts)
+user_options = summer(*user_options_str.split())
+
+
+def get_window_info(options: int=user_options, relative_to: bool=kCGNullWindowID) -> list:
+    return CGWindowListCopyWindowInfo(options, relative_to)
+
+
+def gen_ids_from_info(windows: iter) -> iter:
+    for win_dict in windows:
+        owner = win_dict.get('kCGWindowOwnerName', '')
+        name = win_dict.get('kCGWindowName', '')
+        num = win_dict.get('kCGWindowNumber', '')
+        yield num, owner, name
+
+
+def print_window_ids(windows: iter):
+    for info in windows:
+        print(*info)
+
+
+def gen_window_ids(parent: str, title: str='', *args, options=str) -> iter:
+    options = summer(*options.split(' '))
+    windows = get_window_info(options)
+    parent, title = parent.lower(), title.lower()
+
+    for num, owner, name in gen_ids_from_info(windows):
+        if parent in owner.lower():
+            if title:
+                if title in name.lower():
+                    yield num
+            else:
+                yield num
---
+++ screencapture.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+from subprocess import getstatusoutput
+
+import click
+
+from get_window_id import gen_window_ids, options, user_options_str
+
+
+@click.command()
+@click.option('-t', '--title', default=None, help="Title of window from APPLICATION_NAME to capture.")
+@click.option('-f', '--filename', default=None, help="Filename to save the captured PNG as.")
+@click.option('-w', '--window_selection_options', default=user_options_str, help="Options: " + ' '.join(option for option in options))
+@click.argument('application_name')
+def screenshot_window(application_name: str, title: str=None, filename: str=None, window_selection_options: str=None, **kwargs):
+    if not filename:
+        filename = '_'.join(map(str, (application_name, title, datetime.now()))) + '.png'
+
+    windows = list(gen_window_ids(application_name, title, window_selection_options))
+
+    if windows:
+        window = windows[0]
+
+    else:
+        raise ValueError("Window with parent %s and title %s not found." % (application_name, title))
+
+    rc, output = getstatusoutput('screencapture -l %s "%s"' % (window, filename))
+
+    if rc == 0:
+        print(filename)
+
+        return filename
+
+    else:
+        raise Exception("Error in screencapture command %s: %s", (rc, output))
+
+
+if __name__ == "__main__":
+    screenshot_window()
