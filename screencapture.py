a211c7749fb86facf397b43f490283f4d7a3f825
alex <alex.delorenzo@gmail.com>
2015-03-09 00:44:35 -0400

    add detailed description

--- screencapture.py
+++ screencapture.py
@@ -23,7 +23,7 @@ def _filename(*args) -> str:
 @click.option('-t', '--title', default=None, help="Title of window from APPLICATION_NAME to capture.")
 @click.option('-f', '--filename', default=None, help="Filename to save the captured PNG as.")
 @click.option('-w', '--window_selection_options', default=user_options_str,
-              help="Options: " + ' '.join(option for option in options))
+              help="Options: " + ' '.join(option for option in options) +'\nDefault: ' + user_options_str)
 @click.option('-a', '--all_windows', is_flag=True, default=False)
 @click.argument('application_name')
 def screenshot_window(application_name: str, title: str='', filename: str='', window_selection_options: str='',
