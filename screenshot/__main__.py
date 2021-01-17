7feef2ebf8636bbea9adca9d0c6ac5beadbda473
Alex DeLorenzo <3579286+alexdelorenzo@users.noreply.github.com>
2021-01-17 02:35:34 -0500

    Add basic platform detection and warnings

--- screenshot/__main__.py
+++ screenshot/__main__.py
@@ -1,4 +1,12 @@
-from .mac.capture import run
+from detect import mac, linux, windows
+
+
+if mac:
+  from .mac.capture import run
+
+else:
+  raise Exception("Your operating system isn't supported by screenshot.")
+
 
 if __name__ == "__main__":
   run()
